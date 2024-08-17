e385642781ad856e979cafe1edcc6fa85007bd57
Anna Glid <anna.glid27@gmail.com>
2024-08-17 15:48:25 +0200

    layout done

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class App:
         self.root = tb.Window(themename="cosmo")
         self.root.title("See MIDI")
         self.root.geometry('{}x{}'.format(*self.root.maxsize()))
+        self.root.state('zoomed')
         self.root_frame = ScrolledFrame(self.root, autohide=True)
         self.root_frame.pack(fill=tb_const.BOTH, expand=tb_const.YES, padx=10, pady=10)        
 
@@ -91,70 +92,32 @@ class App:
         self.fret_range_frame.grid(row=2, padx=30, pady=10)
         #endregion
 
-        self.scale_root_frame = tb.Labelframe(self.settings_frame, text=strings['scale_root'])
-        self.input_scale_root = tb.Combobox(self.scale_root_frame, width=10, state='readonly',
+        #region root scale
+        self.scale_root_frame = tb.Labelframe(self.settings_frame, text=strings['scale_root'], bootstyle="secondary")
+        self.input_scale_root = tb.Combobox(self.scale_root_frame, width=7, state='readonly',
                                              values=constants['all_notes'])
         self.input_scale_root.current(0)
-        self.input_scale_root.grid()
-        self.scale_root_frame.grid(row=2, column=2, ipadx=10, ipady=20)
-        # label_scale_type = tk.Label(self.root_frame, text=strings['type'])
-        self.input_scale_type = ttk.Combobox(self.root_frame, width=15, state='readonly',
+        self.input_scale_root.grid(pady=10, padx=20)
+        self.scale_root_frame.grid(row=2, column=2, padx=10, pady=20)
+        #endregion 
+
+        #region scale type
+        self.scale_type_frame = tb.Labelframe(self.settings_frame, text=strings['type'], bootstyle="secondary")
+        self.input_scale_type = tb.Combobox(self.scale_type_frame, width=15, state='readonly',
                                              values=[strings[s] for s in constants['scale_types']])
         self.input_scale_type.current(0)
+        self.input_scale_type.grid(pady=10, padx=30)
+        self.scale_type_frame.grid(row=2, column=3, padx=20, pady=20)
+        #endregion
+
+        # ok button
+        self.btn_update = tb.Button(self.settings_frame, text=strings['update'], command=self.show_guitar_fretboard,
+                                    bootstyle="primary, outline", takefocus=False)
+        self.btn_update.grid(row=2, column=4, padx=10, pady=5)
 
 
         self.settings_frame.grid(row=row, sticky='news', padx=20, pady=10)
         row += 1
-        #endregion
-     
-
-
-        # self.btn_record = tk.Button(self.root_frame, text=strings['start_recording'])
-        # self.btn_pause = tk.Button(self.root_frame, text=strings['pause_recording'])
-        # self.btn_stop = tk.Button(self.root_frame, text=strings['stop_recording'])
-        # self.btn_update = ttk.Button(self.root_frame, text=strings['update'], command=self.show_guitar_fretboard)
-        
-        #endregion
-
-        #region pack widgets
-        # row = 0
-        # row += 1
-        # self.root_frame.grid(sticky='nsew', row=row, column=0)
-        # row += 1        
-        # self.check_show_guitar.grid(row=row)
-        # row += 1
-        # row += 1             
-        # self.check_show_piano.grid(row=row, column=0)
-        # row += 1
-        # self.piano_grid_params = {
-        #     "row": row, "column": 0, "columnspan": 4
-        # }
-        # if self.check_state_show_piano.get():
-        #     self.piano.canvas.grid(**self.piano_grid_params)
-        # row += 1 
-        # # self.btn_record.grid(row=row, column=1, padx=5)
-        # # self.btn_pause.grid(row=row, column=2, padx=5)
-        # # self.btn_stop.grid(row=row, column=3, padx=5)
-        # # row += 1
-        # sep = tk.Label(self.root_frame, text="", pady=10)
-        # sep.grid(row=row, column=0)
-        # row += 1
-        # label_fret_range.grid(row=row, column=0)
-        # row += 1
-        # label_from.grid(row=row, column=0)
-        # self.input_fret_from.grid(row=row, column=1)
-        # label_to.grid(row=row, column=2)
-        # self.input_fret_to.grid(row=row, column=3)
-        # row += 1
-        # label_scale.grid(row=row, column=0)
-        # row += 1
-        # label_scale_root.grid(row=row, column=0)
-        # self.input_scale_root.grid(row=row, column=1)
-        # label_scale_type.grid(row=row, column=2)
-        # self.input_scale_type.grid(row=row, column=3)
-        # row += 1
-        # self.btn_update.grid(row=row, column=0, pady=5)
-        #endregion
 
         self.show_guitar_fretboard()        
         self.root.mainloop()
