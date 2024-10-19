c9e85183dbc05b30fc5162743cf38fb567fcb5c9
Anna Glid <anna.glid27@gmail.com>
2024-10-19 13:45:34 +0200

    signal config

--- code/signalconfig.py
+++ code/signalconfig.py
@@ -1,16 +1,19 @@
 import mido
 import tkinter as tk
 import ttkbootstrap as tb
+from datetime import datetime, timedelta
 
 from commons import exception_catcher
 
 class SignalConfig:
-    LOG_NBR_LIMIT = 50
+    LOG_NBR_LIMIT = 20
+    TIMEOUT = 3
 
     def __init__(self, settings_client):
         self.settings_client = settings_client
         self.logs = ''
         self.log_nbr = 0
+        self.running = True        
     
     @exception_catcher
     def start(self):
@@ -32,14 +35,34 @@ class SignalConfig:
         btn = tb.Button(self.window, command=self.check_signal, text=self.settings_client.strings['check_signal'],
                         bootstyle="default", takefocus=False)
         btn.grid(row=row)
+        self.window.protocol("WM_DELETE_WINDOW", self.close_window)
+        self.window.mainloop()
 
     @exception_catcher
     def check_signal(self):
+        # any_signal = False
+        self.running = True
+        timeout = datetime.now() + timedelta(seconds=self.TIMEOUT)
         try:
-            inport = mido.open_input()
-            self.add_log(self.settings_client.strings['yes_signal'])
-            for signal in inport.iter_pending():
-                self.add_log(signal)
+            with mido.open_input() as inport:
+                self.add_log(self.settings_client.strings['yes_signal'])
+                while self.running:
+                    if timeout < datetime.now():
+                        self.running = False
+                        print(f'No signal in {self.TIMEOUT} seconds.')
+                        self.add_log(f'No signal in {self.TIMEOUT} seconds.')
+                        break
+                    try:
+                        for signal in inport.iter_pending():
+                            self.add_log(str(signal))
+                            print(signal)
+                            timeout = datetime.now() + timedelta(seconds=self.TIMEOUT)
+                            if not self.running:
+                                break
+                    except Exception as ex:
+                        print(f'Exception: {ex}')
+                        self.add_log(f'Exception: {ex}')
+                        self.running = False
         except OSError:
             self.add_log(self.settings_client.strings['no_signal'])
 
@@ -52,5 +75,13 @@ class SignalConfig:
         self.log_nbr += 1
         self.textbox.insert(tk.END, log + '\n')
         self.textbox.config(state=tk.DISABLED)
+        self.window.update()
+        print(f'log added: {log}')
+
+    def close_window(self):
+        self.running = False  # turn off while loop
+        self.window.destroy()
+        print( "Window closed")
+
 
         
